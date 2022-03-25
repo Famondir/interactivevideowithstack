4ecc99256e494e7372fb42fe84faf9b3e8be468c
Famondir <schaefersd@gmail.com>
2022-03-25 17:33:39 +0100

    Extracted all JS code possible into external script. Question is now centered on videoplayer.

--- interactive video with stack loading script.js
+++ interactive video with stack loading script.js
@@ -2,7 +2,11 @@
 <script src="https://cdn.jsdelivr.net/npm/videojs-youtube@2.6.1/dist/Youtube.min.js"></script>
 <script src="https://videowithstack.4lima.de/stackwithvideo.js"></script>
 
-/* Video einbinden */
+<script type="text/javascript">
+	list = {#list#};
+</script>
+
+/* Video per Direktlink einbinden */
 <div id="interactiveVideo" class = "mt-2 mb-2">
     <video-js id="video1" class="video-js vjs-default-skin" controls
 	data-setup='{"playbackRates": [0.75, 0.9, 1, 1.1, 1.25, 1.5], "fluid": true}'>
@@ -10,30 +14,39 @@
 	</video-js>
 </div>
 
+/* bindet Video von Youtube ein */
 /*
-<div id="interactiveVideo">
+<div id="interactiveVideo" class = "mt-2 mb-2">
 	<video-js id="video1" class="video-js vjs-default-skin" controls
-    data-setup='{"playbackRates": [0.75, 0.9, 1, 1.1, 1.25, 1.5], "fluid": true, "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=xjS6SftYQaQ"}] }'>
-  </video-js>
-
+	data-setup='{"playbackRates": [0.75, 0.9, 1, 1.1, 1.25, 1.5], "fluid": true, "techOrder": ["youtube"], 
+	"sources": [{ "type": "video/youtube", "src": "https://www.youtube.com/watch?v=xjS6SftYQaQ"}] }'>
+	</video-js>
 </div>
 */
 
-/* Anzeige aktueller Aufgabe */
+/* Anzeigeplatzhalter für die aktuelle Aufgabe
+wird per JS auf das Videoelement verschoben
+ */
 <div id="questionCanvas" class="card" style="visibility: hidden;">
-	<h4 class="card-header" id="questionCanvasHeader">aktuelle Aufgabe<button type="button" class="close" onclick="toggleV('questionCanvas'); return false;">
+	<h4 class="card-header" id="questionCanvasHeader">aktuelle Aufgabe
+		<button type="button" class="close" onclick="toggleV('questionCanvas'); return false;"> /* don't reload the page */
           <span aria-hidden="true">&times;</span>
-        </button></h4>
+        </button>
+	</h4>
 	<div class="card-footer" id="questionCanvasFooter">
-  </div>
+		/* hier kommt der submit-Button hin */
+	</div>
 </div>
 
-/* Aufgabensammlung */
+/* Aufgabensammlung
+hier werden alle Aufgaben gestellt
+bereits richtig beantwortete Aufgaben sieht man dann hier (als Archiv)
+*/
 <div id="aufgabensammlung" class="card bg-light" style="display: none;">
 	<div class="card-header">
 		<h4 class="card-title">Richtig beantwortete Aufgaben</h4>
 	</div>
-    <div class="card-body" id="a1" style="display: none;">
+    <div class="card-body" id="a1" style="display: none;"> /* id muss mit erstem Eintrag in Stackliste übereinstimmen */
         <h5 class="card-title">Aufgabe 1</h5>
         <p class="card-text">Was ergibt \({@v1@}+{@v2@}\)?</p>
         <p class="card-text">[[input:ans1]] [[validation:ans1]]</p>
@@ -53,238 +66,7 @@
     </div>
 </div>
 
-<script type="text/javascript">
-	// var video = document.getElementsByTagName("video")[0];
-		
-	var player = videojs("video1");
-    var maxtime = 0;
-	var list = null;
-	var prtList = null;
-	var anzahl = 0;
-	var anzahlRichtig = 0;
-	var aktuelleAufgabe = "";
-
-    var slider = document.getElementById("myRange");
-	
-	list = {#list#};
-	// console.log(list);
-
-    document.addEventListener("DOMContentLoaded", function(event) {
-		console.log('DOM is fully loaded');
-		
-		// Check-Button verschieben
-		/*
-        const checkButtonArea = document.querySelector('.im-controls');
-		checkButtonArea.style.textAlign = "center";
-        checkButtonArea.style.marginBottom = ".5em";
-		checkButtonArea.style.backgroundColor = "red";
-		checkButtonArea.id = "pruefenButton";
-		*/
-		
-		const checkButton = document.querySelector('input[type="submit"]')
-		checkButton.classList.add("btn-block");
-		checkButton.onclick = function() {uebertrage()};
-		checkButton.style.margin = 0;
-		checkButton.style.borderRadius = 0;
-		
-		document.getElementById('questionCanvasFooter').insertAdjacentElement('beforeend', checkButton);
-
-		// Aufgabenstatus updaten
-		prtList = document.getElementsByClassName("stackprtfeedback");
-		var prtArray = Array.prototype.slice.call(prtList).filter(function (el) {return !el.classList.contains("stackprtfeedback-prtZ")});
-		anzahl = prtArray.length;
-		
-		list.push(["none",player.duration()]);
-		//console.log(list);
-		
-		anzahlRichtig = 0;
-		for (let i = 0; i < anzahl; i++) {
-			//  console.log(prtArray[i].children[0].className=="correct");
-			if (prtArray[i].children[0].className=="correct") {
-				anzahlRichtig+=1;
-			}
-			
-			document.getElementById(list[i][0]).style.display = "block";
-		}
-		
-		//console.log("AnzhalRichtig: ", anzahlRichtig);
-		
-		aktuelleAufgabe = list[anzahlRichtig][0];
-		maxtime = list[anzahlRichtig][1];
-		
-		//document.getElementById(list[anzahl][0]).style.display = "block";	
-		
-		getState();
-		
-		// Neueste Aufgabe verschieben
-        if (anzahlRichtig < list.length-1) {
-			var currentQuestion = document.getElementById(list[anzahlRichtig][0]);
-			currentQuestion.classList.add("overflow-auto");
-			document.getElementById('questionCanvas').children[0].insertAdjacentElement('afterend', currentQuestion);
-		}
-		
-		// Aufgabensammlung anzeigen, wenn nötig
-		if (anzahlRichtig > 0) {
-			document.getElementById("aufgabensammlung").style.display = "block";
-		}
-    });
-		
-
-	// video.addEventListener("timeupdate", stateQuestion, false);
-	
-	// verhindert vorspulen (Quelle: https://stackoverflow.com/a/39754847/14406173)	
-	var timeTracking = {
-		watchedTime: 0,
-		currentTime: 0
-	};
-				
-	var lastUpdated = 'currentTime';
-
-	window.onload = (event) => {
-		console.log('page is fully loaded');
-		// document.getElementsByClassName("vjs-big-play-button")[0].style.display = "none";
-		// document.getElementsByClassName("vjs-control-bar")[0].style.display = "flex";
-		
-		// document.querySelector('input[type="submit"]').onclick = function() {uebertrage()};
-		
-		const playerCanvas = document.querySelector(".video-js")
-		const questionCanvas = document.querySelector("#questionCanvas");
-		
-		var pCWidth = playerCanvas.clientWidth;
-		var pCHeight = playerCanvas.clientHeight;
-		var qCWidth = playerCanvas.clientWidth;
-		var qCHeight = playerCanvas.clientHeight;
-		
-		console.log("Width: "+pCWidth);
-		
-		playerCanvas.appendChild(questionCanvas);
-		questionCanvas.style.cssText = `
-		visibility: hidden;
-		position: absolute; 
-		top: 5%; 
-		left: 5%; 
-		max-width: 90%;
-		max-height: 80%;
-		font-size: .92345rem;
-		font-weight: 400;
-		line-height: 1.5;
-		text-align: left;
-		color: #000;
-		`;
-		
-		if (anzahlRichtig < anzahl) {
-			showV("questionCanvas");
-		}
-	};
-	
-	player.ready(function(){        
-		console.log('player ready');
-		
-		this.on('loadedmetadata', function(){		
-			console.log('metadata loaded');
-			
-			list[list.length-1] = (["none",player.duration()]);
-			maxtime = list[anzahlRichtig][1];
-			//console.log('list?');
-			
-			// setzt Videozeit auf letzten Videostand
-			if (document.querySelector('#zaehler')) {
-				var currTime = (document.querySelector('#zaehler').innerHTML > maxtime ? maxtime : document.querySelector('#zaehler').innerHTML);
-				console.log("Setze Video fort bei: ", currTime);
-			} else {
-				var currTime = 0;
-			}
-			
-			timeTracking["watchedTime"] = currTime;
-			timeTracking["currentTime"] = currTime;
-			player.currentTime(currTime);
-			
-			addMarkers(); // manchmal wurden die Marker nicht richtig eingefügt. Eiun spezieller Observer hat geholfen
-			
-			player.play(); // spiele nach erstem Check automatisch weiter
-
-			 // ändere das Abspielsymbol passend zum Playerstatus
-			waitForElm('.vjs-play-control').then((elm) => {
-				if (!player.paused) {
-					elm.classList.add('vjs-playing');
-				} else {
-					elm.classList.add('vjs-paused');
-				}
-			});
-		
-		});		
-		
-		this.on('timeupdate', function () {
-			if (!player.seeking()) {
-				if (player.currentTime() > timeTracking.watchedTime) {
-					timeTracking.watchedTime = player.currentTime();
-					lastUpdated = 'watchedTime';
-				}
-				//tracking time updated  after user rewinds
-				else {
-					timeTracking.currentTime = player.currentTime();
-					lastUpdated = 'currentTime';
-				}
-				
-				// console.log("state q");
-				stateQuestion();
-			}
-			
-		});
-		
-		// prevent user from seeking
-		this.on('seeking', function () {
-			// guard against infinite recursion:
-			// user seeks, seeking is fired, currentTime is modified, seeking is fired, current time is modified, ....
-			// var delta = video.currentTime - timeTracking.watchedTime;
-			var delta = player.currentTime() - maxtime;
-			if (delta > 0) {
-				player.pause();
-				console.log("Pause");
-				//play back from where the user started seeking after rewind or without rewind
-				player.currentTime((timeTracking[lastUpdated] < maxtime ? timeTracking[lastUpdated] : maxtime)); // soll Endlosschleife vorbeugen
-				player.play();
-			}
-		});
-		
-		var Button = videojs.getComponent('Button');
-		var MyButton = videojs.extend(Button, {
-		  constructor: function() {
-			Button.apply(this, arguments);
-			// initialize your button
-			this.addClass('stack-question-button');
-			this.controlText("toggle question visibility");
-		  },
-		  handleClick: function() {
-			// do something on click
-			toggleV('questionCanvas');
-		  }
-		});
-		videojs.registerComponent('MyButton', MyButton);
-		var player = videojs('video1');		
-		player.getChild('controlBar').addChild('myButton', {}, 1);
-		//document.getElementsByClassName("vjs-current-time")[0].insertBefore('myButton', {});
-		
-		/* Fügt Checkmarkicon auf PLayerbutton hinzu */
-		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').classList.add("fa-svg-icon");
-		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').classList.add("svg-baseline");
-		var svgCM = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-		svgCM.setAttributeNS(null, 'viewBox', '0 0 1792 1792');
-		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').appendChild(svgCM);
-		var pathCM = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-		pathCM.setAttributeNS(null, 'd', 'M1472 930v318q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h832q63 0 117 25 15 7 18 23 3 17-9 29l-49 49q-10 10-23 10-3 0-9-2-23-6-45-6h-832q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113v-254q0-13 9-22l64-64q10-10 23-10 6 0 12 3 20 8 20 29zm231-489l-814 814q-24 24-57 24t-57-24l-430-430q-24-24-24-57t24-57l110-110q24-24 57-24t57 24l263 263 647-647q24-24 57-24t57 24l110 110q24 24 24 57t-24 57z');
-		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').children[0].appendChild(pathCM);
-	});
-	
-	/*
-	jQuery( document ).ready(function() {
-		console.log("jQuery funnktioniert. document ready");
-		
-		
-	});
-	*/
-</script>
-
 <div id="zahl" style="display: none;">
 [[input:ansZ]][[validation:ansZ]][[feedback:prtZ]]
 </div>
+
--- stackwithvideo.js
+++ stackwithvideo.js
@@ -9,7 +9,7 @@
  * @author Simon Schäfer <Simon.Schaefer@HTW-Berlin.de>
  *
  * Created at     : 2022-02-16
- * Last modified  : 2022-02-24
+ * Last modified  : 2022-03-25
  */
 
 // #----------# defining functions #----------#
@@ -165,8 +165,187 @@ loadCSS(cssId, 'https://vjs.zencdn.net/7.18.0/video-js.css');
 cssId = 'interactivePlayerCSS'
 loadCSS(cssId, 'https://videowithstack.4lima.de/stackwithvideo.css');
 
+// #----------# defining global variables #----------#
+
+var player = null;
+var maxtime = 0;
+var list = null;
+var prtList = null;
+var anzahl = 0;
+var anzahlRichtig = 0;
+var aktuelleAufgabe = "";
+
 // #----------# adding eventlisteners #----------#
 
+// Seitenstruktur ist geladen, Bilder etc. aber noch nicht
+document.addEventListener("DOMContentLoaded", function(event) {
+	console.log('DOM is fully loaded');
+	
+	// fügt der in STACK definierten Liste der Aufgaben einen Knoten hinzu, der die maximale Videolaufzeit enthalten wird
+	// dafür müssen aber erst die Metadaten des Videos geladen sein
+	list.push(["none", NaN]);
+	
+	// setzt die Referenz zum vjs player
+	player = videojs("video1");
+	
+	// Check-Button verschieben		
+	const checkButton = document.querySelector('input[type="submit"]')
+	checkButton.classList.add("btn-block");
+	checkButton.classList.add("btn-block-full");
+	checkButton.onclick = function() {uebertrage()};
+	document.getElementById('questionCanvasFooter').insertAdjacentElement('beforeend', checkButton);
 
+	// Aufgabenstatus updaten
+	// zählt, wie viele Feedbackfelder von STACK schon freigegeben wurden, da eine Eingabe für die Aufgabe erfolgte
+	// bezieht aber das versteckte Feedbackfeld prtZ nicht mit
+	prtList = document.getElementsByClassName("stackprtfeedback");
+	var prtArray = Array.prototype.slice.call(prtList).filter(function (el) {return !el.classList.contains("stackprtfeedback-prtZ")});
+	anzahl = prtArray.length;
+	
+	anzahlRichtig = 0;
+	for (let i = 0; i < anzahl; i++) {
+		//  console.log(prtArray[i].children[0].className=="correct");
+		if (prtArray[i].children[0].className=="correct") {
+			anzahlRichtig+=1;
+		}
+		
+		// schaltet die bereits richtig gelösten Aufgaben und die neue Aufgabe sichtbar
+		showD(list[i][0]);
+	}
+	
+	aktuelleAufgabe = list[anzahlRichtig][0];
+	maxtime = list[anzahlRichtig][1];
+	
+	getState();
+	
+	// wenn der Player initialisiert wurde...
+	player.ready(function(){        
+		console.log('player ready');
+		
+		// wird genutzt, um das vorspulen zu verhindern (Quelle: https://stackoverflow.com/a/39754847/14406173)	
+		var lastUpdated = 'currentTime';
+		var timeTracking = {
+			watchedTime: 0,
+			currentTime: 0
+		};
+		
+		// wenn die Videolänge festgestellt wurde...
+		this.on('loadedmetadata', function(){		
+			console.log('metadata loaded');
+			
+			// schreibt die Videogesamtlänge in die erweiterte Liste
+			list[list.length-1] = (["none",player.duration()]);
+			maxtime = list[anzahlRichtig][1];
+			
+			// setzt Videozeit auf letzten Videostand
+			if (document.querySelector('#zaehler')) {
+				var currTime = (document.querySelector('#zaehler').innerHTML > maxtime ? maxtime : document.querySelector('#zaehler').innerHTML);
+				console.log("Setze Video fort bei: ", currTime);
+			} else {
+				var currTime = 0;
+			}
+			
+			timeTracking["watchedTime"] = currTime;
+			timeTracking["currentTime"] = currTime;
+			player.currentTime(currTime);
+			
+			addMarkers(); // fügt der prograss-bar die Kapitelmarken und den schwarzen Balken hinzu
+			player.play(); // spiele nach erstem Check automatisch weiter; quasi autoplay
+
+			// ändere das Abspielsymbol passend zum Playerstatus
+			waitForElm('.vjs-play-control').then((elm) => {
+				if (!player.paused) {
+					elm.classList.add('vjs-playing');
+				} else {
+					elm.classList.add('vjs-paused');
+				}
+			});
+		
+		});		
+		
+		// wird genutzt, um das vorspulen zu verhindern (Quelle: https://stackoverflow.com/a/39754847/14406173)	
+		// wenn sich die Videozeit ändert...
+		this.on('timeupdate', function () {
+			// ohne dass der benutzer eingreift...
+			if (!player.seeking()) {
+				if (player.currentTime() > timeTracking.watchedTime) {
+					timeTracking.watchedTime = player.currentTime();
+					lastUpdated = 'watchedTime';
+				} else {
+					timeTracking.currentTime = player.currentTime();
+					lastUpdated = 'currentTime';
+				}
+				
+				stateQuestion(); // prüft, ob neue Frage angezeigt werden muss
+			}
+		});
+		
+		// wenn der Benutzer im Video springen möchte...
+		this.on('seeking', function () {
+			// guard against infinite recursion:
+			// user seeks, seeking is fired, currentTime is modified, seeking is fired, current time is modified, ....
+			var delta = player.currentTime() - maxtime;
+			if (delta > 0) {
+				player.pause();
+				console.log("Video pausiert, da zu weit gesprungen");
+				//play back from where the user started seeking after rewind or without rewind
+				player.currentTime((timeTracking[lastUpdated] < maxtime ? timeTracking[lastUpdated] : maxtime)); // soll Endlosschleife vorbeugen
+				player.play();
+			}
+		});
+		
+		// Fügt dem Player einen Button hinzu, mit dem die Aufgabe angezeigt und ausgeblendet werden kann
+		var Button = videojs.getComponent('Button');
+		var MyButton = videojs.extend(Button, {
+		  constructor: function() {
+			Button.apply(this, arguments);
+			// initialize your button
+			this.addClass('stack-question-button');
+			this.controlText("toggle question visibility");
+		  },
+		  handleClick: function() {
+			// do something on click
+			toggleV('questionCanvas');
+		  }
+		});
+		
+		videojs.registerComponent('MyButton', MyButton);	
+		player.getChild('controlBar').addChild('myButton', {}, 1);
+		
+		// Fügt Checkmarkicon auf Playerbutton hinzu
+		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').classList.add("svg-icon");
+		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').classList.add("svg-baseline");
+		var svgCM = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		svgCM.setAttributeNS(null, 'viewBox', '0 0 1792 1792');
+		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').appendChild(svgCM);
+		var pathCM = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		pathCM.setAttributeNS(null, 'd', 'M1472 930v318q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h832q63 0 117 25 15 7 18 23 3 17-9 29l-49 49q-10 10-23 10-3 0-9-2-23-6-45-6h-832q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113v-254q0-13 9-22l64-64q10-10 23-10 6 0 12 3 20 8 20 29zm231-489l-814 814q-24 24-57 24t-57-24l-430-430q-24-24-24-57t24-57l110-110q24-24 57-24t57 24l263 263 647-647q24-24 57-24t57 24l110 110q24 24 24 57t-24 57z');
+		document.querySelector('.video-js .stack-question-button .vjs-icon-placeholder').children[0].appendChild(pathCM);
+	});
+	
+	// Neueste Aufgabe verschieben
+	if (anzahlRichtig < list.length-1) { // wenn noch nicht alle Aufgaben gelöst wurden
+		var currentQuestion = document.getElementById(list[anzahlRichtig][0]);
+		currentQuestion.classList.add("overflow-auto"); // lässt scrollen, falls Inhalt zu große für Anzeigefeld
+		document.getElementById('questionCanvas').children[0].insertAdjacentElement('afterend', currentQuestion); // verschiebt die Aufgabe in den PLatzhalter
+	}
+	
+	// Aufgabensammlung anzeigen, wenn nötig
+	if (anzahlRichtig > 0) {
+		document.getElementById("aufgabensammlung").style.display = "block";
+	}
+	
+	const playerCanvas = document.querySelector(".video-js")
+	const questionCanvas = document.querySelector("#questionCanvas");
+	playerCanvas.appendChild(questionCanvas);
+	
+	if (anzahlRichtig < anzahl) {
+		showV("questionCanvas");
+	}
+});
+
+window.onload = (event) => {
+	console.log('page is fully loaded');
+};
 
 // #----------#  #----------#
